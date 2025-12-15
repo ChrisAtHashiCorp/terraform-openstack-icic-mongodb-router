8086c58dd3ec3f895da744ae06522e4fdd8cf030
Chris Polansky <cpolansky@hashicorp.com>
2025-12-15 16:50:03 +0000

    Adding ssh defaults

--- variables.tf
+++ variables.tf
@@ -44,7 +44,7 @@ variable "ssh_conn" {
     user     = optional(string, "root")
     password = optional(string, "HashiPass123!")
   })
-  default = {}
+  default = {user = "root", password = "HashiPass123!"}
 }
 
 variable "ssh_bastion" {
